7831f04c6fb09d9fb3ce23dd6071ff3e34d29169
Theodora2000 <xsimont@stuba.sk>
2021-02-25 11:15:16 +0100

    1.2

--- src/main/java/sk/stuba/fei/uim/oop/cv1.java
+++ src/main/java/sk/stuba/fei/uim/oop/cv1.java
@@ -1,7 +1,27 @@
 package sk.stuba.fei.uim.oop;
+import java.util.Scanner;
+
 
 public class cv1 {
+
     public static void main(String[] args){
-        System.out.println("Hello world");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Zadaj cisla:");
+        int a, b;
+        a=input.nextInt();
+        b=input.nextInt();
+
+        System.out.println("Vrati:" + porovnaj(a,b));
+
+    }
+    static int porovnaj(int a , int b){
+        if(a>b){
+            return 1;
+        }else if(a<b){
+            return -1;
+        }else{
+            return 0;
+        }
     }
 }
